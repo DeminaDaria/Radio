48388ccde717e46ffe628d8cfb1b183f785c89a3
Demina Daria <demina.dasha92@gmail.com>
2020-10-05 02:19:06 +0300

    add RadioProject

--- src/main/java/ru/netology/domain/RadioProject.java
+++ src/main/java/ru/netology/domain/RadioProject.java
@@ -1,4 +1,106 @@
 package ru.netology.domain;
 
 public class RadioProject {
+
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int currentStation;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+    private int currentVolume;
+    private boolean on;
+
+    public void increaseStation() {
+        if (currentStation == maxStation){
+            this.currentStation = minStation;
+            return;
+        }
+        currentStation ++;
+    }
+
+    public void decreaseStation() {
+        if (currentStation == minStation){
+            this.currentStation = maxStation;
+            return;
+        }
+        currentStation --;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        currentVolume ++;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume == minVolume){
+            return;
+        }
+        currentVolume --;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public void setMaxStation(int maxStation) {
+        this.maxStation = maxStation;
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public void setMinStation(int minStation) {
+        this.minStation = minStation;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+
+        if (currentStation > maxStation){
+            this.currentStation = maxStation;
+            return;
+        }
+
+        this.currentStation = currentStation;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+
 }
--- src/test/java/ru/netology/domain/RadioProjectTest.java
+++ src/test/java/ru/netology/domain/RadioProjectTest.java
@@ -7,7 +7,120 @@ import static org.junit.jupiter.api.Assertions.*;
 class RadioProjectTest {
 
     @Test
-    void shouldSetCurrentRadioStation
+    public  void shouldClickNextRadioStation() {
+        RadioProject radioProject = new RadioProject();
+
+        radioProject.setCurrentStation(3);
+        radioProject.setOn(true);
+
+        radioProject.increaseStation();
+
+        assertEquals(4, radioProject.getCurrentStation());
+    }
+
+    @Test
+    public  void shouldClickNextRadioStationMax() {
+        RadioProject radioProject = new RadioProject();
+
+        radioProject.setCurrentStation(9);
+        radioProject.setOn(true);
+
+        radioProject.increaseStation();
+
+        assertEquals(0, radioProject.getCurrentStation());
+    }
+
+    @Test
+    public  void shouldClickPrevRadioStation() {
+        RadioProject radioProject = new RadioProject();
+
+        radioProject.setCurrentStation(3);
+        radioProject.setOn(true);
+
+        radioProject.decreaseStation();
+
+        assertEquals(2, radioProject.getCurrentStation());
+    }
+
+    @Test
+    public  void shouldClickPrevRadioStationMin() {
+        RadioProject radioProject = new RadioProject();
+
+        radioProject.setOn(true);
+
+        radioProject.decreaseStation();
+
+        assertEquals(9, radioProject.getCurrentStation());
+    }
+
+    @Test
+    public  void shouldIncreaseCurrentVolume() {
+        RadioProject radioProject = new RadioProject();
+
+        radioProject.setCurrentVolume(6);
+        radioProject.setOn(true);
+
+        radioProject.increaseVolume();
+
+        assertEquals(7, radioProject.getCurrentVolume());
+    }
+
+    @Test
+    public  void shouldIncreaseCurrentVolumeMax() {
+        RadioProject radioProject = new RadioProject();
+
+        radioProject.setCurrentVolume(10);
+        radioProject.setOn(true);
+
+        radioProject.increaseVolume();
+
+        assertEquals(10, radioProject.getCurrentVolume());
+    }
+
+    @Test
+    public  void shouldDecreaseCurrentVolume() {
+        RadioProject radioProject = new RadioProject();
+
+        radioProject.setCurrentVolume(4);
+        radioProject.setOn(true);
+
+        radioProject.decreaseVolume();
+
+        assertEquals(3, radioProject.getCurrentVolume());
+    }
+
+    @Test
+    public  void shouldDecreaseCurrentVolumeMin() {
+        RadioProject radioProject = new RadioProject();
+
+        radioProject.setOn(true);
+
+        radioProject.decreaseVolume();
+
+        assertEquals(0, radioProject.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldSetNumberStationRemoteControlValid() {
+        RadioProject radioProject = new RadioProject();
+
+        radioProject.setCurrentStation(7);
+        radioProject.setOn(true);
+
+        assertEquals(7,radioProject.getCurrentStation());
+
+    }
+
+    @Test
+    public void shouldSetNumberStationRemoteControlInvalid() {
+        RadioProject radioProject = new RadioProject();
+
+        radioProject.setCurrentStation(10);
+        radioProject.setOn(true);
+
+        assertEquals(9,radioProject.getCurrentStation());
+
+    }
 
 
 }
